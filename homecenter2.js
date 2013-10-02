814b7346378c3e851588b0b91d0519c9920877a4
vincent <vincent.fresnel@gmail.com>
2013-10-02 20:59:44 +0200

    Version 1.3
    - prise en charge des pieces.
    . les modules peuvent avoir le meme nom si ils sont positionnés dans des
    pieces diffentes.
    - prise en charge des portlets avec la version 2.9 et 3.0 de SARAH.

--- homecenter2.js
+++ homecenter2.js
@@ -1,28 +1,62 @@
+exports.init = function (SARAH){
+  status(SARAH.ConfigManager.getConfig());
+}
+
 exports.action = function(data, callback, config){
 	console.log('##### Home Center 2 #####');
 
-var config = config.modules.homecenter2;
+var config = config.modules.homecenter2;	
 
 	switch (data.request)
 	{
 	case 'set':
-	get_modules( set, data, callback, config );
+	get_rooms( set, data, callback, config );
 	break;
 	case 'get':
-	get_modules( get, data, callback, config );
+	get_rooms( get, data, callback, config );
 	break;
 	case 'launch':
-	get_modules( set, data, callback, config );
+	get_rooms( set, data, callback, config );
 	break;
-	case 'update':
-	get_modules(update, data, callback, config );
+	case 'updatedevices':
+	get_rooms(update, data, callback, config );
+	break;
+	case 'updaterooms':
+	get_rooms(update, data, callback, config );
 	break;
 	default:
 	output(callback, "Une erreur s'est produite: ");
 	}
 }
 
-var get_modules = function ( action, data, callback, config ) {
+var get_rooms = function ( action, data, callback, config ) {
+	var http = require('http');
+	var options = {
+	  hostname: config.ip,
+	  port: 80,
+	  path: '/api/rooms',
+	  auth: config.login + ':' + config.password
+	};
+	
+	var request = data.request;
+	http.get(options, function(res) {
+			
+			var buffer = '';
+			res.on('data', function (chunk) {
+					buffer += chunk;
+				});
+			
+			res.on('end', function(){
+					var jsonrooms = JSON.parse(buffer);
+					get_modules(jsonrooms, action, data, callback, config);
+				});
+		}).on('error', function(e) {
+			output(callback, "Une erreur s'est produite: " + e.message);
+		});
+		
+}
+
+var get_modules = function ( jsonrooms, action, data, callback, config ) {
 	var http = require('http');
 	var options = {
 	  hostname: config.ip,
@@ -41,7 +75,7 @@ var get_modules = function ( action, data, callback, config ) {
 			
 			res.on('end', function(){
 					var json = JSON.parse(buffer);
-					get_scenes(json, action, data, callback, config);
+					get_scenes(jsonrooms, json, action, data, callback, config);
 				});
 		}).on('error', function(e) {
 			output(callback, "Une erreur s'est produite: " + e.message);
@@ -49,7 +83,7 @@ var get_modules = function ( action, data, callback, config ) {
 		
 }
 
-var get_scenes = function ( json, action, data, callback, config ) {
+var get_scenes = function ( jsonrooms, json, action, data, callback, config ) {
 	var http = require('http');
 	var options = {
 	  hostname: config.ip,
@@ -67,52 +101,80 @@ var get_scenes = function ( json, action, data, callback, config ) {
 			
 			res.on('end', function(){
 					json = json.concat(JSON.parse(buffer));
-					action(json, data, callback, config);
+					action(jsonrooms, json, data, callback, config);
 				});
 		}).on('error', function(e) {
 			output(callback, "Une erreur s'est produite: " + e.message);
 		});
 }
 
-var get = function ( json, data, callback, config) {
+var get = function (jsonrooms, json, data, callback, config) {
 	console.log("***** GET *****");
 	var text = data.module;
-
-	for ( var i = 0; i < json.length; i++ ) {
-		var module = json[i];
-		var tokens = module.name.split(' ');
-		var found = true;
-		for ( var j = 0; found && j < tokens.length; j++ ) {
-			found = new RegExp(tokens[j],'i').test(text);
+	var text2 = data.room;
+	var value = data.value;
+	
+	for ( var i = 0; i < jsonrooms.length; i++ ) {
+	var rooms = jsonrooms[i];
+	var tokens = rooms.name.split(' ');
+	var found = true;
+	for ( var j = 0; found && j < tokens.length; j++ ) {
+			found = new RegExp(tokens[j],'i').test(text2);
 		}
 
-		console.log ( "Found (" + i + ") " + module.name + ": " + found );
+		console.log ( "Found rooms (" + i + ") " + rooms.name + ": " + found );
 		if ( found ) {
-			return say(module, callback);
+			for ( var i = 0; i < json.length; i++ ) {
+				var module = json[i];
+				var tokens = module.name.split(' ');
+				var found = true;
+				if (rooms.id == module.roomID){
+				for ( var j = 0; found && j < tokens.length; j++ ) {
+					found = new RegExp(tokens[j],'i').test(text);
+				}
+
+			console.log ( "Found modules (" + i + ") " + module.name + ": " + found );
+			if ( found ) {
+				return say(module, callback);
+			}
+		}
+		}
 		}
 	}
+
 	
 	console.log('rien trouvé');
 	return output( callback, 'je ne sais pas');
 }
 
-var set = function ( json, data, callback, config ) {
+var set = function (jsonrooms, json, data, callback, config ) {
 	console.log("***** SET *****");
 	var text = data.module;
+	var text2 = data.room;
 	var value = data.value;
 	
-	for ( var i = 0; i < json.length; i++ ) {
-		var module = json[i];
-
-		var tokens = module.name.split(' ');
-		var found = true;
-		for ( var j = 0; found && j < tokens.length; j++ ) {
-			found = new RegExp(tokens[j],'i').test(text);
+for ( var i = 0; i < jsonrooms.length; i++ ) {
+	var rooms = jsonrooms[i];
+	var tokens = rooms.name.split(' ');
+	var found = true;
+	for ( var j = 0; found && j < tokens.length; j++ ) {
+			found = new RegExp(tokens[j],'i').test(text2);
 		}
 
-		console.log ( "Found (" + i + ") " + module.name + ": " + found );
-		if ( found ) {
-			if ( data.request == "set") {
+		console.log ( "Found rooms (" + i + ") " + rooms.name + ": " + found );
+		if ( found ) {	
+			for ( var i = 0; i < json.length; i++ ) {
+				var module = json[i];
+				var tokens = module.name.split(' ');
+				var found = true;
+				if (rooms.id == module.roomID){
+				for ( var j = 0; found && j < tokens.length; j++ ) {
+					found = new RegExp(tokens[j],'i').test(text);
+				}
+
+			console.log ( "Found (" + i + ") " + module.name + ": " + found );
+			if ( found ) {
+				if ( data.request == "set") {
 						console.log('module action');
 					var http = require('http');
 					var options = {
@@ -121,8 +183,8 @@ var set = function ( json, data, callback, config ) {
 					path: '/api/callAction?deviceID='+module.id+"&name=setValue&arg1="+get_value(module,value),
 					auth: config.login + ':' + config.password
 					};
-			} else {
-			console.log('run scene');
+				} else {
+				console.log('run scene');
 					var http = require('http');
 					var options = {
 					hostname: config.ip,
@@ -142,7 +204,7 @@ var set = function ( json, data, callback, config ) {
 					res.on('end', function(){
 						setTimeout((function() {				
 						if ( data.request == "set") {
-						get_modules( get, data, callback, config );
+						get_rooms( get, data, callback, config );
 						} else {
 						output(callback, "fait");
 						}
@@ -154,6 +216,9 @@ var set = function ( json, data, callback, config ) {
 			
 			return 
 		}
+	}
+	}
+	}
 	}
 
 	console.log('rien trouvé');
@@ -172,31 +237,50 @@ var get_value = function ( module, value ) {
 			return value;
 			break;
 		}
+		case 'blind':
+		if ( value == 'true' || value == 'false') {
+		return (value == 'false'? 0: 99);
+		} else {
+			return value;
+			break;
+		}
 	}
 }
 
 var say = function ( module, callback ) {
 	switch ( module.type ) {
 		case 'temperature_sensor':
-			output (callback, 'la température est de ' + module.properties.value + get_unit(module));
+			output (callback, 'la ' + module.name + ' est de ' + module.properties.value + get_unit(module));
 			break;
 		case 'binary_light':
-			output (callback, "c'est " + (module.properties.value == '0'? ' éteint': ' allumé'));
+			var string = module.name + " est " + (module.properties.value == '0'? ' éteint': ' allumé');
+			if (module.properties.valueSensor && module.properties.valueSensor !="") 
+				string += ' et la consommation est de ' + returnString(module.properties.valueSensor,".",",") + get_unit(module);
+			output (callback, string);
 			break;
 		case 'dimmable_light':
-			output (callback, module.properties.value == '0'? 'c\'est éteint': 'c\'est allumé à ' + module.properties.value + 'pourcent');
+			output (callback, module.properties.value == '0'? 'c\'est éteint': 'c\'est allumé à ' + module.properties.value + 'pour cent');
 			break;
 		case 'humidity_sensor':
 			output (callback, 'le taux d\'humidité est de ' + module.properties.value + get_unit(module));
 			break;
 		case 'multi_level_sensor':
-			output (callback, 'la valeur est de ' + module.properties.value + get_unit(module));
+			output (callback, 'la valeur de ' + module.name + ' est de ' + module.properties.value + get_unit(module));
 			break;
 		case 'door_sensor':
-			output (callback, module.properties.value == '0'? 'c\'est fermé': 'c\'est ouvert ');
+			output (callback, module.name + (module.properties.value == '0'? ' est fermé': ' est ouvert '));
+			break;
+		case 'light_sensor':
+			output (callback, 'la ' + module.name + ' est de ' + module.properties.value + get_unit(module));
 			break;
 		case 'virtual_device':
 			output (callback, 'test');
+			break;
+		case 'blind':
+			output (callback, module.name + (module.properties.value == '0'? ' est fermé': ' est ouvert '));
+			break;
+		case 'motion_sensor':
+			output (callback, (module.properties.value == '0'? ' pas de mouvements sur ': ' detection présence sur') + module.name );
 			break;
 		default:
 		output(callback, "Je ne peux pas exécuter cette action");
@@ -204,15 +288,20 @@ var say = function ( module, callback ) {
 }
 
 var get_unit = function ( module ) {
-	switch ( module.properties.unit ) {
+var unit = module.properties.unit;
+if (unit == "")
+unit = module.properties.unitSensor
+	switch ( unit ) {
 		case 'W':
 			return ' watt';
 		case '%':
-			return ' pourcent';
+			return ' pour cent';
 		case 'C':
 			return ' degrés';
 		case 'F':
 			return ' degrés';
+		case 'Lux':
+			return ' Lux';
 		default:
 			return ' ';
 	}
@@ -223,7 +312,7 @@ var output = function ( callback, output ) {
 	callback({ 'tts' : output});
 }
 
-var update = function(json, data, callback, config){
+var update = function(jsonrooms, json, data, callback, config){
 	console.log("***** UPDATE  *****");
 
 	if (!data.directory){ 
@@ -247,12 +336,77 @@ var update = function(json, data, callback, config){
 		}
 	replace += '  </one-of>\n';
 	replace += '<!-- §';
+	
+		var replace2  = '@ -->\n';
+	replace2 += '  <one-of>\n';
+					
+		for ( var i = 0; i < jsonrooms.length; i++ ) {
+			var module = jsonrooms[i];
+			var tokens = module.name.split(' ');
+			replace2 += '    <item>'+module.name+'<tag>out.action.room="'+module.name+'"</tag></item>\n';
+			console.log('ajout de : ' + module.name);
+						
+		}
+	replace2 += '  </one-of>\n';
+	replace2 += '<!-- @	';
+						
+	var regexp = new RegExp('§[^§]+§','gm');
+	var regexp2 = new RegExp('@[^@]+@','gm');
+	var xml    = xml.replace(regexp,replace);
+	xml    = xml.replace(regexp2,replace2);
+	
+	fs.writeFileSync(file, xml, 'utf8');
+	fs.writeFileSync(file, xml, 'utf8');
+	
+	var file = data.directory + '/../plugins/homecenter2/portlet.html';
+	var xml  = fs.readFileSync(file,'utf8');
+  
+	var replace  = '§ -->\n';
+	replace += '<BR>nombre de modules = <b>'+json.length+'</b></BR>\n';
+	replace += 'nombre de pieces = <b>'+jsonrooms.length+'</b></BR>\n';
+	replace += '<!-- §';
 						
 	var regexp = new RegExp('§[^§]+§','gm');
 	var xml    = xml.replace(regexp,replace);
 	fs.writeFileSync(file, xml, 'utf8');
 	fs.writeFileSync(file, xml, 'utf8');
 		
-  callback({ 'tts' : 'mise a jour de la base terminée'});
+  callback({ 'tts' : 'j\'ai trouvé ' + json.length + ' modules et ' + jsonrooms.length + ' pieces'});
+  console.log('j\'ai trouvé ' + json.length + ' modules et ' + jsonrooms.length + ' pieces');
 
 }
+
+var returnString = function (data, stringtoreplace, word) {
+var str = data;
+str = str.replace(stringtoreplace,word);
+//console.log('return: ' + str);
+return str;
+}
+
+var status = function (config) {	
+	var config = config.modules.homecenter2;
+	var http = require('http');
+	var options = {
+	  hostname: config.ip,
+	  port: 80,
+	  path: '/api/devices',
+	  auth: config.login + ':' + config.password
+	};
+
+	http.get(options, function(res) {
+			res.on('data', function (chunk) {
+				});
+			
+			res.on('end', function(){
+			
+						console.log("alive");
+			status = 'alive';
+				});
+		}).on('error', function(e) {
+		console.log("dead");
+				status = 'dead';
+		});
+	return status;	
+}
+
+exports.status  = status;
